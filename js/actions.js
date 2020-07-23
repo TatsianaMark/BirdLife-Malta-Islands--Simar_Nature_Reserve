d174aeda9438f2c8c03ba39734cc121504e22d28
TatsianaMark <tanya030890@mail.ru>
2020-07-23 10:10:56 +0200

    feat:add actions to switch photos

--- js/actions.js
+++ js/actions.js
@@ -2,6 +2,39 @@ const popupLinks = document.querySelectorAll('.popup-link');
 const body = document.querySelector('body');
 const lockPadding = document.querySelectorAll(".lock-padding");
 
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    if(n > slides.length){
+        slideIndex = 1
+    }
+    if (n < 1){
+        slideIndex = slides.length
+    }
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display="none";
+
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace("active","");
+    }
+    slides[slideIndex -1].style.display = "block";
+    dots[slideIndex-1].className+= "active";
+}
+
 let unlock = true;
 
 const timeout = 500;
